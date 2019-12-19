99427b4c109780b6c303d2e12147b9e3bbd332ca
daniel <danielbene@protonmail.com>
2019-12-19 05:01:38 -0800

    Fix paths

--- install_scripts/open_eats_install.sh
+++ install_scripts/open_eats_install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-HOST_IP=`grep -m 1 HOST_IP ../docker_compose/.env | cut -d '=' -f 2-`
-OPENEATS_DIR=`grep -m 1 OPENEATS_DIR ../docker_compose/.env | cut -d '=' -f 2-`
-OPENEATS_GUI_PORT=`grep -m 1 OPENEATS_GUI_PORT ../docker_compose/.env | cut -d '=' -f 2-`
-OPENEATS_API_PORT=`grep -m 1 OPENEATS_API_PORT ../docker_compose/.env | cut -d '=' -f 2-`
+HOST_IP=`grep -m 1 HOST_IP docker_compose/.env | cut -d '=' -f 2-`
+OPENEATS_DIR=`grep -m 1 OPENEATS_DIR docker_compose/.env | cut -d '=' -f 2-`
+OPENEATS_GUI_PORT=`grep -m 1 OPENEATS_GUI_PORT docker_compose/.env | cut -d '=' -f 2-`
+OPENEATS_API_PORT=`grep -m 1 OPENEATS_API_PORT docker_compose/.env | cut -d '=' -f 2-`
 
 # -------------------------------------
 
@@ -13,20 +13,20 @@ BASE_DIR=`pwd`
 BACKUP_MODE=false
 mkdir -p $OPENEATS_DIR
 
-sed 's/HOST_IP/'$HOST_IP'/g' ../configs/openeats/env_prod.list.sample > ../configs/openeats/env_prod.list
-sed -i 's/OPENEATS_GUI_PORT/'$OPENEATS_GUI_PORT'/g' ../configs/openeats/env_prod.list
-sed -i 's/OPENEATS_API_PORT/'$OPENEATS_API_PORT'/g' ../configs/openeats/env_prod.list
-sed 's/OPENEATS_GUI_PORT/'$OPENEATS_GUI_PORT'/g' ../configs/openeats/docker-prod.override.yml.sample > ../configs/openeats/docker-prod.override.yml
-sed -i 's/OPENEATS_API_PORT/'$OPENEATS_API_PORT'/g' ../configs/openeats/docker-prod.override.yml
+sed 's/HOST_IP/'$HOST_IP'/g' configs/openeats/env_prod.list.sample > configs/openeats/env_prod.list
+sed -i 's/OPENEATS_GUI_PORT/'$OPENEATS_GUI_PORT'/g' configs/openeats/env_prod.list
+sed -i 's/OPENEATS_API_PORT/'$OPENEATS_API_PORT'/g' configs/openeats/env_prod.list
+sed 's/OPENEATS_GUI_PORT/'$OPENEATS_GUI_PORT'/g' configs/openeats/docker-prod.override.yml.sample > configs/openeats/docker-prod.override.yml
+sed -i 's/OPENEATS_API_PORT/'$OPENEATS_API_PORT'/g' configs/openeats/docker-prod.override.yml
 
 cd $OPENEATS_DIR
 git clone https://github.com/open-eats/OpenEats.git
 
 cd $BASE_DIR
-cp ../configs/openeats/docker-prod.override.yml $OPENEATS_DIR/OpenEats/docker-prod.override.yml
-cp ../configs/openeats/env_prod.list $OPENEATS_DIR/OpenEats/env_prod.list
-if [ -f ../configs/openeats/openeats.sql ]; then
-	cp ../configs/openeats/openeats.sql $OPENEATS_DIR/OpenEats/
+cp configs/openeats/docker-prod.override.yml $OPENEATS_DIR/OpenEats/docker-prod.override.yml
+cp configs/openeats/env_prod.list $OPENEATS_DIR/OpenEats/env_prod.list
+if [ -f configs/openeats/openeats.sql ]; then
+	cp configs/openeats/openeats.sql $OPENEATS_DIR/OpenEats/
 	BACKUP_MODE=true
 fi
 
