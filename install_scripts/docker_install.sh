e8ca1706628c8375078ee012b6bc612963c81766
Daniel Bene <danielbene@protonmail.com>
2019-12-29 12:00:41 +0100

    Added base distro handling for docker install

--- install_scripts/docker_install.sh
+++ install_scripts/docker_install.sh
@@ -4,8 +4,8 @@ sudo apt-get remove -y docker docker-engine docker.io containerd runc
 
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+curl -fsSL https://download.docker.com/linux/HOST_BASE_DIST/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/HOST_BASE_DIST $(lsb_release -cs) stable"
 
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
--- terraform_srv.sh
+++ terraform_srv.sh
@@ -3,6 +3,7 @@
 # ---ENV_VARS--------------------------
 
 HOST_IP=`grep -m 1 HOST_IP docker_compose/.env | cut -d '=' -f 2-`
+HOST_BASE_DIST=`grep -m 1 HOST_BASE_DIST docker_compose/.env | cut -d '=' -f 2-`  # this should be ubuntu/debian according to what your OS based on (others may work if there is a docker repo for them)
 SCRAPE_INTERVAL=`grep -m 1 SCRAPE_INTERVAL docker_compose/.env | cut -d '=' -f 2-`
 PROM_CONF_DIR=`grep -m 1 PROM_CONF_DIR docker_compose/.env | cut -d '=' -f 2-`
 
@@ -18,6 +19,7 @@ sudo mkdir -p `grep -m 1 SDP_CONF_DIR docker_compose/.env | cut -d '=' -f 2-`
 
 sed 's/HOST_IP/'$HOST_IP'/g' configs/prometheus_sample.yml > configs/prometheus.yml
 sed -i 's/SCRAPE_INTERVAL/'$SCRAPE_INTERVAL'/g' configs/prometheus.yml
+sed -i 's/HOST_BASE_DIST/'$HOST_BASE_DIST'/g' install_scripts/docker_install.sh
 cp configs/prometheus.yml $PROM_CONF_DIR
 
 # ---INSTALLS--------------------------
