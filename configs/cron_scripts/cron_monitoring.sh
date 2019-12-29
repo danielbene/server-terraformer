ef01e9b00a899f90f92a33835432e9baddc7385e
Daniel Bene <danielbene@protonmail.com>
2019-12-29 12:27:55 +0100

    Remove unecessary files

--- configs/cron_scripts/cron_monitoring.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/home/daniel/configs/monitoring/textfile-collectors/node-exporter-textfile-collector-scripts/smartmon.sh | sponge /var/lib/node_exporter/textfile_collector/smartmon.prom
